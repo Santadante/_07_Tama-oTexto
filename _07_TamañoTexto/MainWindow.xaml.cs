cd849ef5b40e549f325ee585bb684dda779ab6fd
abc <abc@example.com>
2020-10-22 17:22:14 +0200

    Agregar archivos de proyecto.

---
+++ _07_TamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace _07_TamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void pequeñoRadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton r = (RadioButton)sender;
+            textoTextBlock.FontSize = int.Parse(r.Tag.ToString());
+        }
+    }
+}
